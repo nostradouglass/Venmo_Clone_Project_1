3acc84a8bc2bef80658e218cb51a6a638e2aa2ff
Kelly Douglass <kdouglass@gmail.com>
2019-12-30 18:39:05 -0600

    Tab Bar basics working. Need to figure out small layout bug. layout of subfragments is going to the top of the tabs area not, not startign below the tabs.

--- app/src/main/java/com/kellydouglass/venmocloneproject1/SocialSlider.java
+++ app/src/main/java/com/kellydouglass/venmocloneproject1/SocialSlider.java
@@ -1,20 +1,38 @@
 package com.kellydouglass.venmocloneproject1;
 
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.tabs.TabLayout;
+import com.kellydouglass.venmocloneproject1.TabFragmentSocial.FriendsFragment;
+import com.kellydouglass.venmocloneproject1.TabFragmentSocial.MeFragment;
+import com.kellydouglass.venmocloneproject1.TabFragmentSocial.PublicFragment;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class SocialSlider extends Fragment {
+public class SocialSlider extends Fragment  {
 
+
+    ViewPager viewPager;
+    TabLayout tabLayout;
+
+    FriendsFragment friendsFragment;
+    MeFragment meFragment;
+    PublicFragment publicFragment;
 
     public SocialSlider() {
         // Required empty public constructor
@@ -27,5 +45,63 @@ public class SocialSlider extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_social_slider, container, false);
     }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        viewPager = view.findViewById(R.id.viewPager);
+        tabLayout = view.findViewById(R.id.tabLayout);
+
+        friendsFragment = new FriendsFragment();
+        meFragment = new MeFragment();
+        publicFragment = new PublicFragment();
+
+        MyPageAdapter myPageAdapter = new MyPageAdapter(getActivity(), getChildFragmentManager());
+
+        tabLayout.setupWithViewPager(viewPager);
+        viewPager.setAdapter(myPageAdapter);
+
+    }
+
+    class MyPageAdapter extends FragmentPagerAdapter {
+
+        Context ctxt=null;
+
+        String[] fragmentNames = {"first", "second", "third"};
+
+        public MyPageAdapter(Context ctxt, FragmentManager fm) {
+            super(fm);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+
+            switch (position) {
+                case 0:
+                    return friendsFragment;
+                case 1:
+                    return meFragment;
+                case 2:
+                    return publicFragment;
+            }
+
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return fragmentNames.length;
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return fragmentNames[position];
+        }
+    }
+
 
 }
